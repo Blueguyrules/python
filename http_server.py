2b965572a7861c90600f6a2ba0e3195e1b060354
John Clark <inindev@gmail.com>
2017-09-10 20:12:11 -0400

    simple http server example

---
+++ http_server.py
@@ -0,0 +1,27 @@
+
+import datetime
+from BaseHTTPServer import BaseHTTPRequestHandler, HTTPServer
+
+
+class MyHandler(BaseHTTPRequestHandler):
+	def do_GET(self):
+		self.send_response(200)
+		self.send_header('content-type', 'text/html')
+		self.end_headers()
+		self.wfile.write('<p>current date: ' + str(datetime.datetime.now().date()) + '</p>\n')
+		self.wfile.write('<p>current time: ' + str(datetime.datetime.now().time()) + '</p>\n')
+		return
+
+def main():
+	try:
+		print('starting http server...')
+		server = HTTPServer(('', 8080), MyHandler)
+		print('http server started')
+		server.serve_forever()
+	except KeyboardInterrupt:
+		print('exiting...')
+		server.socket.close()
+
+if __name__ == '__main__':
+	main()
+
